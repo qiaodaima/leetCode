9a571d7ac6187e1c0b1d5e69c92f79c9079696ad
qiaodaima <qiaodaima@qq.com>
2023-03-20 20:42:19 +0800

    344. 反转字符串

---
+++ 344. 反转字符串/index.ts
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode.cn/problems/reverse-string/
+ */
+
+const reverseString = (s: string[]): void => {
+  let l = 0;
+  let r = s.length - 1;
+
+  while (l < r) {
+    const temp = s[l];
+
+    s[l] = s[r];
+    s[r] = temp;
+
+    l++;
+    r--;
+  }
+};
